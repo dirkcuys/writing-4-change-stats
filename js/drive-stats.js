656e824cafaa7e41a503e05d96a0b824ac247f9e
Dirk Uys <dirkcuys@gmail.com>
2014-08-19 13:16:43 -0500

    Get all files

--- js/drive-stats.js
+++ js/drive-stats.js
@@ -62,5 +62,31 @@ function makeApiCall() {
       document.getElementById('content').appendChild(heading);
     });
   });
+
+  gapi.client.load('drive', 'v1', function(){
+    retrieveAllFiles(function(result){
+      document.getElementById('filelist').appendChild(result);
+    });
+  });
 }
 
+
+// List all documents in the drive
+function retrieveAllFiles(callback) {
+  var retrievePageOfFiles = function(request, result) {
+    request.execute(function(resp) {
+      result = result.concat(resp.items);
+      var nextPageToken = resp.nextPageToken;
+      if (nextPageToken) {
+        request = gapi.client.drive.files.list({
+          'pageToken': nextPageToken
+        });
+        retrievePageOfFiles(request, result);
+      } else {
+        callback(result);
+      }
+    });
+  }
+  var initialRequest = gapi.client.drive.files.list();
+  retrievePageOfFiles(initialRequest, []);
+}
